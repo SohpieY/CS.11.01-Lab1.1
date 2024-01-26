7547636402e0884c899b80a84ec8abe72674b4a6
tianlinsy <tianlinsy2026@student.cis.edu.hk>
2024-01-26 11:16:23 +0800

    Done!

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner newAge = new Scanner(System.in) ;
+        System.out.println("Enter your age");
+        int myAge;
+        myAge = newAge.nextInt();
+        int secondAge = (7 + (myAge)/2);
+        System.out.println(myAge + " year olds should date somebody who is at least " + secondAge + " years old");
+        String quote  = " Thanks to Jim Cohoon at The University of Virginia";
+        System.out.println("\nCredit:" + quote);
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner theAge = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = theAge.nextInt();    // 20
+        int bookPage = 100 - age;
+        System.out.println(age + " year olds should read at least " + bookPage + " pages before giving up on a book.");
+        String quote = " Thanks to Jim Cohoon at The University of Virginia";
+        System.out.println("\nCredit:" + quote);
     }
 }
